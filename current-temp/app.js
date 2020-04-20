cab583914531aad6963669917dfbd4b6f0e2db71
Richard Davidson <richard@automateers.com>
2020-04-20 23:53:57 +0200

    [master] refactor lambda connection to mongo

--- current-temp/app.js
+++ current-temp/app.js
@@ -5,95 +5,57 @@ const MONGODB_URI = "mongodb+srv://" + process.env.MONGODB_USER + ":" + process.
     process.env.MONGODB_URI + "/vopak?retryWrites=true&w=majority"
 const moment = require('moment');
 
+let cachedDb;
 let response;
 
-function queryDatabase (db) {
-  console.log('=> query database');
+function connectToDatabase (uri) {
+  console.log('=> connect to database');
 
-  return db.collection('items').find({}).toArray();
+  if (cachedDb) {
+    console.log('=> using cached database instance');
+    return Promise.resolve(cachedDb);
+  }
+
+  return MongoClient.connect(uri)
+    .then(db => {
+      cachedDb = db;
+      return cachedDb;
+    });
 }
 
-async function getWeatherItems(client, city) {
-    let q = {'city': city}
-    return client
-        .db()
-        .collection('weather')
+function getMostRecentWeatherItem(client, q) {
+  console.log('=> getMostRecentWeatherItem');
+
+  return client.db().collection('weather')
         .find(q, {
             "limit": 1,
             "sort": [
                 ['ts', 'desc']
             ]
         })
-        .toArray();
+        .toArray()
+    .then(result => {
+        return result
+    });
 }
 
-exports.lambdaHandler = async (event, context, callback) => {
+exports.lambdaHandler = (event, context, callback) => {
     context.callbackWaitsForEmptyEventLoop = false;
-    let db = await MongoClient.connect(MONGODB_URI, { useUnifiedTopology: true });
-    let items = await getWeatherItems(db, 'Covilha');
 
-    response = {
-        statusCode: 200,
-        body: JSON.stringify({
-            temperature: items[0].temperature,
-            current_dt: moment().format(),
-        }),
-    };
-
-    return response
-
-  // connectToDatabase(MONGODB_URI)
-  //   .then(db => queryDatabase(db))
-  //   .then(result => {
-  //     console.log('=> returning result: ', result);
-  //     callback(null, result);
-  //   })
-  //   .catch(err => {
-  //     console.log('=> an error occurred: ', err);
-  //     callback(err);
-  //   });
+    connectToDatabase(MONGODB_URI)
+    .then(db => getMostRecentWeatherItem(db, {'city': 'Covilha'}))
+    .then(result => {
+        response = {
+            statusCode: 200,
+            body: JSON.stringify({
+                temperature: result[0].temperature,
+                current_dt: moment().format(),
+            }),
+        };
+      callback(null, response);
+    })
+    .catch(err => {
+      console.log('=> an error occurred: ', err);
+      callback(err);
+    });
 };
-
-/**
- *
- * Event doc: https://docs.aws.amazon.com/apigateway/latest/developerguide/set-up-lambda-proxy-integrations.html#api-gateway-simple-proxy-for-lambda-input-format
- * @param {Object} event - API Gateway Lambda Proxy Input Format
- *
- * Context doc: https://docs.aws.amazon.com/lambda/latest/dg/nodejs-prog-model-context.html 
- * @param {Object} context
- *
- * Return doc: https://docs.aws.amazon.com/apigateway/latest/developerguide/set-up-lambda-proxy-integrations.html
- * @returns {Object} object - API Gateway Lambda Proxy Output Format
- * 
- */
-// exports.lambdaHandler = async (event, context) => {
-//
-//     // try {
-//         const mongoUri = "mongodb+srv://" + process.env.MONGODB_USER + ":" + process.env.MONGODB_PASSW + "@" +
-//             process.env.MONGODB_URI + "/test?retryWrites=true&w=majority"
-//
-//         const client = new MongoClient(mongoUri, { useNewUrlParser: true });
-//         client.connect(err => {
-//             if (err) {
-//                 console.error(err)
-//             }
-//             const collection = client.db("vopak").collection("weather");
-//             console.log('test')
-//             console.log(collection)
-//             // perform actions on the collection object
-//             client.close();
-//             response = {
-//                 'statusCode': 200,
-//                 'body': JSON.stringify({
-//                     message: 'current temperature',
-//                     timestamp: moment().format()
-//                 })
-//             }
-//
-//             return response
-//         });
-//     // } catch (err) {
-//     //     console.log(err);
-//     //     return err;
-//     // }
-// };
